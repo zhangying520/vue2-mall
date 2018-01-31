d05bad066d7bd71ceac764ecfa62b90c2961b387
DDD <www.124780540@qq.com>
2018-01-31 18:07:29 +0800

    只有生产环境使用延迟加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 const _import = require('./_import_' + process.env.NODE_ENV)
-// _import('login/index')
+// example: _import('login/index')
 // in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
 // detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
 // 在开发环境中，不要使用延迟加载，因为延迟加载太多页面会导致webpack热更新太慢。所以只有在生产中使用延迟加载
+// 修改组件内容后，热更新只会在打开当前组件的页面才会更新
+
 import HelloWorld from '@/components/HelloWorld'
-import GoodsList from '@/views/GoodsList/'
-import GoodsDetail from '@/views/GoodsDetail/'
-import Login from '@/views/Login/index'
-import ErrorHtml from '@/views/404'
-import Cart from '@/views/Cart/'
-import Address from '@/views/Address/'
+// import GoodsList from '@/views/GoodsList/'
+// import GoodsDetail from '@/views/GoodsDetail/'
+// import Login from '@/views/Login/index'
+// import ErrorHtml from '@/views/404'
+// import Cart from '@/views/Cart/'
+// import Address from '@/views/Address/'
 
 Vue.use(Router)
 
