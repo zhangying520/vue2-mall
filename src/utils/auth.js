cbc54b2f419ee6ef88ef3a233a65bd213729b45d
DDD <www.124780540@qq.com>
2018-03-09 11:31:28 +0800

    修改存储cookies方法

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -6,8 +6,8 @@ export function getToken () {
   return Cookies.get(TokenKey)
 }
 
-export function setToken (token) {
-  return Cookies.set(TokenKey, token)
+export function setToken (token, time) {
+  return Cookies.set(TokenKey, token, { expires: time || '' })
 }
 
 export function removeToken () {
